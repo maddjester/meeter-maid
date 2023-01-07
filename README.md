3fd892b65ad6471dff76b8b0619f0b980903458c
maddjester <65257955+maddjester@users.noreply.github.com>
2023-01-06 23:23:11 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# meeter-maid
+ A simple python program that you can drop into your windows startup folder that will detect Voicemeeter Potato's annoying popup DOS attack and instantly close all open Voicemeeter windows.
---
+++ app.py
@@ -0,0 +1,34 @@
+from time import sleep
+
+from clicknium import clicknium as cc
+from clicknium import locator, ui
+from pymem import Pymem, process
+
+handle = "voicemeeter8x64.exe"
+address = (0x135708)
+window = locator.voicemeeter8x64.register_window
+
+def skip_wait():
+    '''
+    Edits VoiceMeeter memory address for the countdown timer and re-writes it to 0.
+    '''
+    pm = Pymem(handle)
+    module = process.module_from_name(pm.process_handle, handle).lpBaseOfDll
+    value = pm.read_int(module + address)
+    if value > 0 and value < 40:
+        pm.write_int(module + address, 0)
+
+if __name__ == "__main__":
+    while True:
+        if cc.is_existing(window):
+            skip_wait()
+            main_win = ui(window).parent.parent
+            child_wins = main_win.children
+            try:
+                for win in child_wins:
+                    win.send_hotkey("%{F4}", preaction="click")
+            except Exception as err:
+                print(err)
+            finally:
+                main_win.send_hotkey("%{F4}", preaction="click")
+        sleep(0.1)
