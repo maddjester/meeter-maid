e37deba074d84049da65868e1266dbcf7b1b1294
maddjester <65257955+maddjester@users.noreply.github.com>
2023-04-21 12:01:38 -0500

    Fixed Probblem where the program didn't execute until 40 seconds were left

--- app.py
+++ app.py
@@ -16,20 +16,26 @@ def skip_wait():
     pm = Pymem(handle)
     module = process.module_from_name(pm.process_handle, handle).lpBaseOfDll
     value = pm.read_int(module + address)
-    if value > 0 and value < 40:
+    if value > 0 < 999:
         pm.write_int(module + address, 0)
+        return True
+    return False
 
 if __name__ == "__main__":
+    main_win = None
     while True:
-        if cc.is_existing(window):
-            skip_wait()
-            main_win = ui(window).parent.parent
-            child_wins = main_win.children
-            try:
-                for win in child_wins:
-                    win.send_hotkey("%{F4}", preaction="click")
-            except Exception as err:
-                print(err)
-            finally:
-                main_win.send_hotkey("%{F4}", preaction="click")
-        sleep(1)
+        if main_win == None:
+            if cc.is_existing(window):
+                res = skip_wait()
+                if res == True:
+                    main_win = ui(window).parent.parent
+                    child_wins = main_win.children
+                    try:
+                        for win in child_wins:
+                            win.send_hotkey("%{F4}", preaction="click")
+                    except Exception as err:
+                        print(err)
+                    finally:
+                        main_win.send_hotkey("%{F4}", preaction="click")
+                        main_win = None
+        sleep(5)
