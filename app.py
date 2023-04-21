db32ac35f1d510bf43db94283d5bdddc5e9f7061
maddjester <65257955+maddjester@users.noreply.github.com>
2023-04-21 12:38:49 -0500

    Fixed issue where program didn't work optimally

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from pymem import Pymem, process
 
 handle = "voicemeeter8x64.exe"
 address = (0x135708)
-
 window = locator.voicemeeter8x64.register_window
 
 def skip_wait():
@@ -14,9 +13,11 @@ def skip_wait():
     Edits VoiceMeeter memory address for the countdown timer and re-writes it to 0.
     '''
     pm = Pymem(handle)
-    module = process.module_from_name(pm.process_handle, handle).lpBaseOfDll
-    value = pm.read_int(module + address)
-    if value > 0 < 999:
+    if pm:
+        module = process.module_from_name(pm.process_handle, handle).lpBaseOfDll
+        if module and address:
+            value = pm.read_int(module + address)
+    if value > 0:
         pm.write_int(module + address, 0)
         return True
     return False
@@ -24,18 +25,16 @@ def skip_wait():
 if __name__ == "__main__":
     main_win = None
     while True:
+        sleep(3)
         if main_win == None:
             if cc.is_existing(window):
-                res = skip_wait()
-                if res == True:
+                if skip_wait() == True:
                     main_win = ui(window).parent.parent
                     child_wins = main_win.children
                     try:
                         for win in child_wins:
                             win.send_hotkey("%{F4}", preaction="click")
-                    except Exception as err:
-                        print(err)
                     finally:
                         main_win.send_hotkey("%{F4}", preaction="click")
                         main_win = None
-        sleep(5)
+        
